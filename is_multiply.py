5a218a7cb495763193d14821de5f765c5e144a46
Anuradha Ghosh <anuradha.srivastava5@gmail.com>
2019-01-18 23:30:40 -0600

    is_multiply.py added in the repo

---
+++ is_multiply.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Sep 10 12:31:20 2018
+
+@author: Anuradha
+"""
+
+def is_multiple(n,m):
+    if n%m ==0:
+        return True
+    else:
+        return False
+if __name__ == '__main__':
+    print(is_multiple(4567392,2))
+
